fd69ee67cc5e3cc2a8ec82721544601afaaf7667
Xiang Ying <yingxiang835@gmail.com>
2014-11-17 21:25:46 +0800

    lex ok!

--- code/lex_permutation.h
+++ code/lex_permutation.h
@@ -1,12 +1,38 @@
 #ifndef LEX_PERMUTATION_H
 #define LEX_PERMUTATION_H
+#include <iostream>
+
+#include <algorithm>
 
 namespace lex{
     template <class T> bool next_permutation(T first, T last){
         if(first == last){
             return false;
         }
-        return true;
+        T right = last - 1;
+        if (right == first){
+        	return false;
+        }
+        while (right > first){
+        	if (*right > *(right - 1))
+        		break;
+        	else
+        		right--;
+        }
+        if (right == first){
+        	std::reverse(first, last);
+        	return false;
+        }else{
+        	//
+        	for (T temp = last - 1;temp >= right;temp--){
+        		if (*(temp) > *(right - 1)){
+        			std::iter_swap(temp, right - 1);
+            		std::reverse(right, last);
+            		return true;
+        		}
+        	}
+        }
+        return false;
     }
 }
 
