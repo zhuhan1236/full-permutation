2be3ba445bd5e7a190f35ccbf180ef5e9071d7d9
zhuhan <zhuhan10@gmail.com>
2014-11-17 20:46:36 +0800

    debuf

--- code/main.cc
+++ code/main.cc
@@ -14,7 +14,7 @@ int main(int argc, char *argv[]){
     int test[] = {1, 2, 3};
     switch(argv[1][0]){
         case 'l':
-            get_full_permutation(lex::next_permutation, &test, &test + 3);
+            get_full_permutation(lex::next_permutation, test, test + 3);
             break;
         case 'i':
             break;
