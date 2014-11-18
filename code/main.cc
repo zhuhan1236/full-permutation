eee96681068dfbf6c51712e84a2c024bb6b54aae
zhuhan <zhuhan10@gmail.com>
2014-11-18 20:04:44 +0800

    ins complete

--- code/main.cc
+++ code/main.cc
@@ -10,22 +10,27 @@ using std::cout;
 using std::endl;
 
 template <class T> void get_full_permutation(bool (*next_permutation)(T, T), T first, T last);
+template <class T> void get_full_permutation(bool (*next_permutation)(T, T, int *, T), T first, T last, T sortedFirst);
 long getSystemTime();
 
 int main(int argc, char *argv[]){
-    char test[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l'};
+    char test[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
+    char temp[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
+    std::reverse(temp, temp + 12);
+
     switch(argv[1][0]){
         case 'l':
-            get_full_permutation(lex::next_permutation, test, test + 12);
+            get_full_permutation(lex::next_permutation, test, test + 10);
             break;
         case 'i':
+            get_full_permutation(ins::next_permutation, test, test + 10, temp);
             break;
         case 'd':
             break;
         case 'e':
             break;
         default:
-            get_full_permutation(std::next_permutation, test, test + 12);
+            get_full_permutation(std::next_permutation, test, test + 10);
     }
     return 0;
 }
@@ -35,6 +40,17 @@ template <class T> void get_full_permutation(bool (*next_permutation)(T, T), T f
     while(next_permutation(first, last));
     long end_time = getSystemTime();
     
+    cout << end_time - start_time << endl;
+}
+
+template <class T> void get_full_permutation(bool (*next_permutation)(T, T, int *, T), T first, T last, T sortedFirst){
+    int *i_number = new int[last - first - 1];
+    memset(i_number, 0, sizeof(int) * (last - first - 1));
+    long start_time = getSystemTime();
+    while(next_permutation(first, last, i_number, sortedFirst));
+    long end_time = getSystemTime();
+    
+    delete [] i_number;
     cout << end_time - start_time << endl;
 }
 
